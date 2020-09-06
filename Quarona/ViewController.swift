e2053ad60585e3486a58f9a66f63f229bc9345f9
meshachadoe <marlienri.avehas@gmail.com>
2020-09-06 16:35:06 +0700

    Initial Commit

---
+++ Quarona/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quarona
+//
+//  Created by Meshach Adoe on 06/09/20.
+//  Copyright © 2020 Meshach Adoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
